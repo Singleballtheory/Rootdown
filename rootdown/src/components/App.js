03fcbb2f649f44511e14ab09e7c90572dd97bb73
Scott Hutley <scotthutley1@comcast.net>
2021-11-19 11:31:42 -0800

    WIP add Drink.js

--- rootdown/src/components/App.js
+++ rootdown/src/components/App.js
@@ -1,11 +1,17 @@
 import React from "react";
+import Drink from "./Drink";
 import Header from "./Header";
 
 function App() {
   return (
     <React.Fragment>
+      <div>
       <Header />
       <hr/>
+      </div>
+      <div>
+      <Drink />
+      </div>
     </React.Fragment>
   );
 }
---
+++ rootdown/src/components/Drink.js
@@ -0,0 +1,28 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+function Drink(props) {
+  return (
+    <React.Fragment>
+      <div onClick={() => props.whenDrinkClicked(props.id)}>
+        <h2>
+          {props.name} - {props.price}
+        </h2>
+        <p>{props.description}</p>
+        <p>{props.pints}</p>
+        <hr />
+      </div>
+    </React.Fragment>
+  );
+}
+
+Drink.propTypes = {
+  name: PropTypes.string,
+  price: PropTypes.number,
+  description: PropTypes.string,
+  pints: PropTypes.number,
+  id: PropTypes.string,
+  whenItemClicked: PropTypes.func
+};
+
+export default Drink;
